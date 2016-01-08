a1e0a475d6b79b73da032384e4706e966dd48a4b
Feihong Hsu <feihong.hsu@gmail.com>
2016-01-07 22:53:22 -0600

    Show compass data in real time

--- hello-world/app/pages/compass/compass.js
+++ hello-world/app/pages/compass/compass.js
@@ -1,4 +1,6 @@
-import {Page} from 'ionic/ionic';
+import {Page} from 'ionic/ionic'
+import {Observable} from 'rxjs/Observable'
+
 
 const COMPASS_LABELS = new Map([
   ['Magnetic heading', 'magneticHeading'],
@@ -10,13 +12,39 @@ const COMPASS_LABELS = new Map([
   templateUrl: 'build/pages/compass/compass.html'
 })
 export class Compass {
-  constructor() {
+  watchId = null
+
+  onPageDidEnter() {
     this.clearCompassData()
+
+    this.getCompassHeadings(200).subscribe(heading => {
+      this.showCompassData(heading)
+    })
+  }
+  onPageDidLeave() {
+    if (this.watchId) {
+      navigator.compass.clearWatch(this.watchId)
+    }
   }
   clearCompassData() {
     this.itemList = [...COMPASS_LABELS].map(pair => {
       let [label, key] = pair
       return {label: label, value: '-'}
+    })
+  }
+  showCompassData(heading) {
+    this.itemList = [...COMPASS_LABELS].map(pair => {
+      let [label, key] = pair
+      let value = (heading[key] !== null) ? heading[key].toFixed(2) : 'N/A'
+      return {label: label, value: value}
+    })
+  }
+  getCompassHeadings(frequency) {
+    return Observable.create(observer => {
+      let onSuccess = (heading) => observer.next(heading)
+      let onError = (error) => alert(`Compass error code: ${error.code}`)
+      this.watchId = navigator.compass.watchHeading(
+        onSuccess, onError, {frequency: frequency})
     })
   }
 }
